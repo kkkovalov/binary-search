f42b7dccd06a0c7d779d5e4dfd162530e98923a9
kkkovalov <vlad_kovalov@icloud.com>
2023-03-20 13:31:50 -0700

    Recursive binary search

--- index.cpp
+++ index.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+//               0 1 2 3 4  5  6  7  8  9
+int array[10] = {1,2,7,9,16,19,20,65,78,91};
+
+int binary_search(int left, int right, int queryValue){
+    int mid = (right+left)/2;
+    std::cout<<"Left: "<<left<<" Mid: "<<mid<<" Right: "<<right<<std::endl;
+
+    if(array[mid] == queryValue) {std::cout<<"Found: "<<mid<<std::endl; return mid;}
+    else if(left == right) return -1;
+    else if(queryValue > array[mid]) {
+        std::cout<<"Move right"<<std::endl;
+        return binary_search(mid + 1, right, queryValue);
+    } else if(queryValue < array[mid]){
+        std::cout<<"Move left"<<std::endl;
+        return binary_search(left, mid - 1, queryValue);
+    }
+    return -1;
+};
 
 int main(void){
-    int a;
+    int searchValue = 16;
+    std::cout<<binary_search(0, (sizeof(array)/4)-1, searchValue)<<std::endl;
 };
